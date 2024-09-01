10784a5b0d98687d3b790a85bc590270b6d186e3
shalom bergman <shalom195295@gmail.com>
2024-09-01 17:09:18 +0300

    correction to namespase

--- api/assetsApi/Data/ApplicationDbContext.cs
+++ api/assetsApi/Data/ApplicationDbContext.cs
@@ -2,19 +2,24 @@
 using Microsoft.EntityFrameworkCore;
 
 namespace assetsApi.Data;
-
-public class ApplicationDbContext : DbContext
 {
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    public class ApplicationDbContext : DbContext
     {
-        Database.EnsureCreated();
-    }
-    public DbSet<Weapon> Weapons { get; set; }
-    public DbSet<Soldier> Soldiers { get; set; }
-    public DbSet<Vehicle> Vehicles { get; set; }
-    public DbSet<Users> Users { get; set; }
-    public DbSet<Unit> Units { get; set; }
 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Soldier> Soldiers { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Unit> Units { get; set; }
+
+    }
 }
+
+
 
