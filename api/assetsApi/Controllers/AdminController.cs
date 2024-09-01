f729407a5ffe6c01889f587207a03a27d4aaf1bd
beni oren <benioren123beni@gmail.com>
2024-09-01 17:39:13 +0300

    add

--- api/assetsApi/Controllers/AdminController.cs
+++ api/assetsApi/Controllers/AdminController.cs
@@ -1,11 +1,46 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace assetsApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AdminController : ControllerBase
     {
+        
+        private readonly UserServic _userservic;
+
+        public AdminController(UserServic ServicUser)
+        {
+
+            _userservic = ServicUser;
+        }
+        
+        [HttpPost]
+        public async Task< IActionResult> AddAdmin(Users user)
+        {
+
+            if (user == null)
+            {
+
+                return BadRequest();
+
+            }
+            bool result =   await _userservic.CheckUserBool(user);
+            if (!result)
+            {
+                await _userservic.CreateUser(user);
+                return StatusCode(StatusCodes.Status201Created, new { messege = "User registered successfully" });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, new { erorr_messege = "You are already registered in the system " });
+            }  
+
+        }
+
+
     }
 }
