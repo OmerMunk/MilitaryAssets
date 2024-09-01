72650658dfd6c9636fed9e9810a140fc287a8dc5
chezkyk <Y0548532123@GMAIL.COM>
2024-09-01 17:20:31 +0300

    Added Get All Active Wepons function in controler and in the service

--- api/assetsApi/Services/WeaponsServices.cs
+++ api/assetsApi/Services/WeaponsServices.cs
@@ -2,10 +2,15 @@
 {
     public class WeaponsServices
     {
-        public async Task<List<Weapon>> GetWeponByUnit(Guid guidUnit)//
+        public async Task<List<Weapon>> GetWeponByUnit(Guid guidUnit)
         {
-            var weponByUnit = await _DbService.GetWeponByUnitFromDB(guidUnit);
+            var weponByUnit = await _DbService.GetWeponByUnitFromDb(guidUnit);
             return weponByUnit;
+        }
+        public async Task<List<Weapon>> GetAllActiveWepons()
+        {
+            var activeWeponList = _DbService.GetAllActiveWeponsFromDb();
+            return activeWeponList;
         }
     }
 }
