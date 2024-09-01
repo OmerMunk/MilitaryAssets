0f441ec9118cea32dc92c15c117a5a11e91bb2ef
Zev Paneth <z0504167424@gmail.com>
2024-09-01 17:07:26 +0300

    The controller of the create function and the service

---
+++ api/assetsApi/Controllers/Vechiles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace assetsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Vechiles : ControllerBase
+    {
+        private readonly VehiclesService _vehiclesService;
+
+        public Vechiles(VehiclesService vehiclesService)
+        {
+            _vehiclesService = vehiclesService;
+        }
+        // POST api/<Vechiles>
+        [HttpPost]
+        public StatusCodeResult Post([FromBody] Vehicle vehicle)
+        {
+            if (_vehiclesService.IsCreated)
+            {
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            return StatusCode(StatusCodes.Status400BadRequest);
+        }
+    }
+}
