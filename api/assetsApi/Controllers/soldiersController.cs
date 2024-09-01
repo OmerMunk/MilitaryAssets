e47b6c5e24e747177dd78e65d953f7bcbab29a30
yps997 <yairps997@gmail.com>
2024-09-01 17:39:56 +0300

    controller soldiers

---
+++ api/assetsApi/Controllers/soldiersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace assetsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+
+    public class soldiersController : ControllerBase
+    {
+       
+        //get all soldiers
+        [HttpGet]
+        public async Task<IActionResult> GetAllSoldier()
+        {
+            return Services.GetAllSoldiers();
+        }
+
+
+        // get specific soldier
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSoldierById([FromBody] Guid id)
+        {
+          return Services.GetSoldierById(id);
+        }
+
+
+        //create soldier
+        [HttpPost]
+        public async Task<IActionResult> CreateSoldier([FromBody] Soldier soldier)
+        {
+            return Services.CreateSoldiers;
+        }
+
+
+        //update soldier
+        [HttpPut("{id}/update")]
+        public async Task<IActionResult> UpdateSoldier([FromBody] Guid id, Soldier soldier)
+        {
+           return services.UpdateSoldier(id, soldier);
+        }
+
+
+        // update status "delete"
+        [HttpPut("{id}/Status")]
+        public async Task<IActionResult> ChangeSoldierStatus([FromBody] Guid id, bool status)
+        {
+            return services.updateStatus(id, status);
+        }
+
+    }
+
+}
