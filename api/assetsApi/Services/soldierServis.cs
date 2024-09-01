4a4d904096bcee4cbf7e3e988aaec8b74ea084ad
Eli Fish <elifish607@gmail.com>
2024-09-01 17:03:26 +0300

    I created Create, GetAll and GetOne

---
+++ api/assetsApi/Services/soldierServis.cs
@@ -0,0 +1,38 @@
+﻿namespace assetsApi.Services
+{
+    public class soldierServis
+    {
+        private readonly ApplicationDbContext _context;
+
+        public soldierServis(ApplicationDbContext mosadDBContext)
+        {
+            _context = mosadDBContext;
+        }
+
+        public async Task Create(Soldier soldier)
+        {
+            _context.Soldiers.Add(soldier);
+            _context.SaveChanges();
+        }
+
+
+        public async Task<List<Soldier>> GetAll()
+        {
+            Task<List<Soldier>> soldiers = new Task<List<Soldier>>();
+            var sol = await _context.Soldiers.ToListAsync();
+            foreach (Soldier soldier in sol)
+            {
+                soldiers.Add(soldier);
+            }
+            return soldiers;
+        }
+
+
+        public async Task<Soldier> GetOne(Guid id)
+        {
+            Soldier soldier = await  _context.Soldiers.FindAsync(id);
+            return soldier;
+        }
+
+    }
+}
