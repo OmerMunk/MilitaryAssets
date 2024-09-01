fb24d3c20cfef015c0a785654b050d25e0f007b9
Shalom Raichman <shalomr1770@gmail.com>
2024-09-01 17:03:01 +0300

    Add Vehicle model

--- api/assetsApi/Models/Vehicle.cs
+++ api/assetsApi/Models/Vehicle.cs
@@ -1,6 +1,15 @@
-﻿namespace assetsApi.Models
+﻿using assetsApi.Enums;
+
+namespace assetsApi.Models
 {
     public class Vehicle
     {
+        public Guid Id { get; set; }
+        public Guid UnitId { get; set; }
+        public string Model { get; set; }
+        public Status Status { get; set; }
+        public string LicensePlate
+        {  get; set; }
+        public string type { get; set; }
     }
 }
