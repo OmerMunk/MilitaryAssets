3981cfdba3dba0f0b0fecfe882673eb63d3200c8
shalom bergman <shalom195295@gmail.com>
2024-09-01 16:56:34 +0300

    creat applicationDbContext and DbSet from models

---
+++ api/assetsApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace assetsApi.Data;
+
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    public DbSet<Weapon> Weapons { get; set; }
+    public DbSet<Soldier> Soldiers { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Users> Users { get; set; }
+    public DbSet<Unit> Units { get; set; }
+
+}
+
