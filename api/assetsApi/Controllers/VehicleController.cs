056280dfc46e5163d9fbe15f344bb4d16e723fc1
rafi053 <rafi053313@gmail.com>
2024-09-01 17:27:05 +0300

    Creating an endpoint that brings all vehicles

---
+++ api/assetsApi/Controllers/VehicleController.cs
@@ -0,0 +1,25 @@
+﻿using assetsApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assetsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly VehicleService _VehicleService;
+
+        public VehicleController(VehicleService vehicleService)
+        {
+            _VehicleService = vehicleService;
+        }
+
+        // קבל כל הרכבים
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vehicle>>> GetAllVehicle()
+        {
+            return await _VehicleService.Vehicle.ToListAsync();
+        }
+    }
+}
