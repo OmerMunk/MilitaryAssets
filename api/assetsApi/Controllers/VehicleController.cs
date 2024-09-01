8a311a7c4d7b6ac1c90838ce012f1416031f3e96
rafi053 <rafi053313@gmail.com>
2024-09-01 17:32:44 +0300

    Creating an end point that brings a vehicle according to ID

--- api/assetsApi/Controllers/VehicleController.cs
+++ api/assetsApi/Controllers/VehicleController.cs
@@ -4,22 +4,36 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace assetsApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class VehicleController : ControllerBase
     {
-        private readonly VehicleService _VehicleService;
+        private readonly VehicleService _vehicleService;
 
         public VehicleController(VehicleService vehicleService)
         {
-            _VehicleService = vehicleService;
+            _vehicleService = vehicleService;
         }
 
         // קבל כל הרכבים
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Vehicle>>> GetAllVehicle()
         {
-            return await _VehicleService.Vehicle.ToListAsync();
+            return await _VehicleService.Vehicles.ToListAsync();
+        }
+
+        // הצג רכב לפי תעודת זהות
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vehicle>> GetEntity(int id)
+        {
+            Vehicle? vehicle = await this._vehicleService.Vehicles.FindAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+
+            return vehicle;
         }
     }
 }
