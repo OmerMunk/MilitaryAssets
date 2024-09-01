d57d499b5ac93e31b98362fcb7661e6a504f22ab
JonathanPerlman <hp0533165306@gmail.com>
2024-09-01 16:24:58 +0300

    Get Functions

---
+++ mvc/assetsMvc/CarService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace assetsMvc
+{
+    public class CarService: BaseService;
+    {
+
+        private string _GetCarByLicensePlateUrl = $"http://localhost:5216/Cars/licensePlate";
+        private string _GetCarsByModelUrl = $"http://localhost:5216/Cars/model";
+        private string _GetCarsByUnitUrl = $"http://localhost:5216/Cars/unit/";
+
+
+        // Get car by LicensePlate
+        public async Task<Car> GetCarByLicensePlateUrl(string LicensePlate)
+        {
+            var response = await _httpClient.GetAsync(_GetCarByLicensePlateUrl + LicensePlate);
+            return response;
+        }
+
+        // Get car by model
+        public async Task<Car> GetCarsByModel(string Model)
+        {
+            var response = await _httpClient.GetAsync(_GetCarsByModelUrl + Model);
+            return response;
+        }
+
+        // Get car by unit
+        public async Task<car> GetCarsByUnit(string unit)
+        {
+            var response = await _httpClient.GetAsync(_GetCarsByUnitUrl + unit);
+            return response;
+        }
+    }
+}
