784e22ea254157520744a1388e0079fd991489cb
chezkyk <Y0548532123@GMAIL.COM>
2024-09-01 17:36:34 +0300

    Added validation

--- api/assetsApi/Controllers/WeaponsController.cs
+++ api/assetsApi/Controllers/WeaponsController.cs
@@ -17,13 +17,23 @@ namespace assetsApi.Controllers
         public async Task<IActionResult> SeveralByUnits(Guid guidUnit)
         {
             var weponByUnitList = await _weaponsService.GetWeponByUnit(guidUnit);
+            if (weponByUnitList == null) { return NotFound(); }
             return StatusCode(StatusCodes.Status200OK, weponByUnitList);
         }
         [HttpGet("activate")]
         public async Task<IActionResult> GetAllActiveWepons()
         {
             var acyiveWeponList = await _weaponsService.GetAllActiveWepons();
+            if (acyiveWeponList == null) { return NotFound(); }
             return StatusCode(StatusCodes.Status200OK, acyiveWeponList);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetWeponById(Guid id)
+        {
+            var weapon = await _weaponsService.GetWeponById(id);
+            if (weapon == null) { return NotFound(); }
+            return StatusCode(StatusCodes.Status200OK, weapon);
         }
     }
 }
