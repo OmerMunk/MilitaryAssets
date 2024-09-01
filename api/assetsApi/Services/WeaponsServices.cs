e4b44e892e4a6016a382d71778cd4c9867b9ba51
chezkyk <Y0548532123@GMAIL.COM>
2024-09-01 17:04:28 +0300

    Added severl by units in service

--- api/assetsApi/Services/WeaponsServices.cs
+++ api/assetsApi/Services/WeaponsServices.cs
@@ -2,5 +2,10 @@
 {
     public class WeaponsServices
     {
+        public async Task<List<Weapon>> GetWeponByUnit(Guid guidUnit)
+        {
+            var weponByUnit = await _DbService.GetWeponByUnitFromDB(guidUnit);
+            return weponByUnit;
+        }
     }
 }
