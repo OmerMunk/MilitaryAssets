e3017cc015b3bc50e0fd07b12ca53878dc3f57b1
Shalom Raichman <shalomr1770@gmail.com>
2024-09-01 17:27:54 +0300

    change the  Vehicle id to nullble

--- api/assetsApi/Models/Vehicle.cs
+++ api/assetsApi/Models/Vehicle.cs
@@ -4,12 +4,11 @@ namespace assetsApi.Models
 {
     public class Vehicle
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public Guid UnitId { get; set; }
         public string Model { get; set; }
         public Status Status { get; set; }
-        public string LicensePlate
-        {  get; set; }
+        public string LicensePlate {  get; set; }
         public string type { get; set; }
     }
 }
